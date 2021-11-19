c53a93846166af39450df828306e21f4f5472909
Roman123-game <82284311+Roman123-game@users.noreply.github.com>
2021-11-19 17:24:53 +0200

    update

--- src/App.js
+++ src/App.js
@@ -2,31 +2,59 @@ import React from 'react';
 import './App.css';
 
 
-
+const data = [];
+var i=0;
 function App() {
-  const data = ["ficus"];
  function addData(){
+  
   const input = document.getElementById("input");
    if (input.value === undefined || input.value == null ){
      console.log("undefined or null")
+     return 0;
    }
   else{
+   
+      data.push(input.value); 
+      console.log(input.value);
+      document.getElementById("parag" + i).innerHTML = data[i];
+     console.log(data);
+     console.log(data[i]);
+    i++;
     
-    data.unshift(input.value);
-    console.log(input.value);
-    const parag = document.getElementById("parag");
-    parag.innerHTML=data[0] ;
-    console.log(data);
-    }
   }
- 
+  
+}
   return (
     <div className="App">
       <p>New Task</p>
       <input className="input" id="input"></input>
-      <button className="button_plus"   onClick={addData}> +</button>
-      <p>List</p>
-      <p className="parag" id="parag">empty</p>
+      <select>
+      <option >Shopping</option>
+      <option>Tech </option>
+      <option>Pleasure</option>
+      </select>
+      <button className ="buttonP" onClick={addData}> +</button>
+      
+      <p id = "shop">Shopping List</p>
+      <p className="parag" id="parag0">empty</p>
+      <p id="parag1" >empty</p>
+      <p id="parag2" >empty</p>
+      <p id="parag3" >empty</p>
+      <p  id="parag4" >empty</p>
+      <hr></hr>
+      <p id = "tech">Tech List</p>
+      <p className="parag" id="parag0">empty</p>
+      <p id="parag1" >empty</p>
+      <p id="parag2" >empty</p>
+      <p id="parag3" >empty</p>
+      <p  id="parag4" >empty</p>
+      <hr></hr>
+      <p id = 'pleasure'>Pleasure List</p>
+      <p className="parag" id="parag0">empty</p>
+      <p id="parag1" >empty</p>
+      <p id="parag2" >empty</p>
+      <p id="parag3" >empty</p>
+      <p  id="parag4" >empty</p>
     </div>
   );
   }
